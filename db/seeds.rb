09b58efdc2724db459baa655603c1ad77445657d
Joshua Lam <joh641@gmail.com>
2013-10-10 19:13:03 -0700

    added hacked badge

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ submissions.each do |submission|
   Submission.create! (submission)
 end
 
-badges = [{:name => 'Test Badge 1', :image => 'test-badge.png', :description => 'Test Badge 1 can be earned by INSERT DESCRIPTION HERE'}, {:name => 'Test Badge 2', :image => 'test-badge.png', :description => 'Test Badge 2 can be earned by INSERT DESCRIPTION HERE'}]
+badges = [{:name => 'Test Badge 1', :image => 'test-badge.png', :description => 'Test Badge 1 can be earned by INSERT DESCRIPTION HERE'}, {:name => 'Hacked', :image => 'test-badge.png', :description => 'Hacked Badge'}]
 
 badges.each do |badge|
   Badge.create! (badge)
