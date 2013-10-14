9c16d6a087977adfe3175ed3cf65e89c4534809b
Josh lam <joh641@gmail.com>
2013-10-14 12:18:53 -0700

    added validations to models

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,10 +1,19 @@
 class Submission < ActiveRecord::Base
   attr_accessible :name, :email, :badge, :status, :date, :description
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :badge, :inclusion => {:in => Submission.all_badges}
+  validates :status, :inclusion => {:in => Submission.all_statuses}
+  validates :date, :presence => true
+  validates :description, :presence => true
   def self.all_badges
     badges = []
     Badge.all.each do |badge|
       badges.push(badge.name)
     end
     badges
+  end
+  def self.all_statuses
+    %w[Approved Pending Rejected]
   end
 end
