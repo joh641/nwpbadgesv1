ddddd5166afe637877573682d89d64a50b7ad294
Josh lam <joh641@gmail.com>
2013-10-16 10:13:40 -0700

    comment out validations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,6 @@
 class Badge < ActiveRecord::Base
   attr_accessible :name, :image, :description, :claimcode
-  validates :name, :presence => true
-  validates :image, :presence => true
-  validates :description, :presence => true
+#  validates :name, :presence => true
+#  validates :image, :presence => true
+#  validates :description, :presence => true
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,11 +1,11 @@
 class Submission < ActiveRecord::Base
   attr_accessible :name, :email, :badge, :status, :date, :description
-  validates :name, :presence => true
-  validates :email, :presence => true
-  validates :badge, :inclusion => {:in => Submission.all_badges}
-  validates :status, :inclusion => {:in => Submission.all_statuses}
-  validates :date, :presence => true
-  validates :description, :presence => true
+#  validates :name, :presence => true
+#  validates :email, :presence => true
+#  validates :badge, :inclusion => {:in => Submission.all_badges}
+#  validates :status, :inclusion => {:in => Submission.all_statuses}
+#  validates :date, :presence => true
+#  validates :description, :presence => true
   def self.all_badges
     badges = []
     Badge.all.each do |badge|
